f9984e7fbcc7a42f64337db9d517625a29e524a2
Marcos González Andrés <marcos@binpar.com>
2016-04-12 00:43:16 +0200

    Update run_app.sh

--- node/scripts/run_app.sh
+++ node/scripts/run_app.sh
@@ -30,7 +30,7 @@ fi
 if [ -f deploy.json ]
 then
   echo "=> Starting node app con forever from deploy.json ..."
-  IS_PROD=1 forever start ./deploy.json
+  IS_PROD=1 /opt/nodejs/bin/forever start ./deploy.json
 else
   echo "=> ERROR: no deploy.json configuration found."
   exit 1
