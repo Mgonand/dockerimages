37ff4505c3f4d762ed88ae77dc8ff72d157cd7bb
Marcos González Andrés <marcos@binpar.com>
2016-08-23 16:35:01 +0200

    Update run_app.sh

--- meteor4/scripts/run_app.sh
+++ meteor4/scripts/run_app.sh
@@ -1,15 +1,61 @@
 set -e
 
+# utilities
+gyp_rebuild_inside_node_modules () {
+  for npmModule in ./*; do
+    cd $npmModule
+
+    isBinaryModule="no"
+    # recursively rebuild npm modules inside node_modules
+    check_for_binary_modules () {
+      if [ -f binding.gyp ]; then
+        isBinaryModule="yes"
+      fi
+
+      if [ $isBinaryModule != "yes" ]; then
+        if [ -d ./node_modules ]; then
+          cd ./node_modules
+          for module in ./*; do
+            (cd $module && check_for_binary_modules)
+          done
+          cd ../
+        fi
+      fi
+    }
+
+    check_for_binary_modules
+
+    if [ $isBinaryModule = "yes" ]; then
+      echo " > $npmModule: npm install due to binary npm modules"
+      rm -rf node_modules
+      npm install
+      # always rebuild because the node version might be different.
+      npm rebuild
+      if [ -f binding.gyp ]; then
+        node-gyp rebuild || :
+      fi
+    fi
+    cd ..
+  done
+}
+
+rebuild_binary_npm_modules () {
+  for package in ./*; do
+    if [ -d $package/node_modules ]; then
+      (cd $package/node_modules && \
+        gyp_rebuild_inside_node_modules)
+    elif [ -d $package/main/node_module ]; then
+      (cd $package/node_modules && \
+        gyp_rebuild_inside_node_modules )
+    fi
+  done
+}
+
 if [ -d /bundle ]; then
   cd /bundle
   tar xzf *.tar.gz
   cd /bundle/bundle/programs/server/
   npm install
-  if [[ -e npm/node_modules/meteor/npm-bcrypt/node_modules/bcrypt ]] ; then
-      rm -rf npm/node_modules/meteor/npm-bcrypt/node_modules/bcrypt
-      npm install --update-binary -f bcrypt
-      cp -r node_modules/bcrypt npm/node_modules/meteor/npm-bcrypt/node_modules/bcrypt
-  fi
   cd /bundle/bundle/
 elif [[ $BUNDLE_URL ]]; then
   cd /tmp
@@ -26,16 +72,25 @@ else
 fi
 
 if [[ $REBUILD_NPM_MODULES ]]; then
-  if [ -f /opt/meteord/rebuild_npm_modules.sh ]; then
-    cd programs/server
-    bash /opt/meteord/rebuild_npm_modules.sh
-    cd ../../
-  else
-    echo "=> Use meteorhacks/meteord:bin-build for binary bulding."
-    exit 1
+  echo "****** Rebuilding npm modules ******"
+  cd /bundle/bundle/programs/server/
+  if [ -d npm ]; then
+    (cd npm && rebuild_binary_npm_modules)
+  fi
+
+  if [ -d node_modules ]; then
+    (cd node_modules && gyp_rebuild_inside_node_modules)
+  fi
+  
+  if [[ -e npm/node_modules/meteor/npm-bcrypt/node_modules/bcrypt ]] ; then
+    echo "******** bcrypt fix ********"
+    rm -rf npm/node_modules/meteor/npm-bcrypt/node_modules/bcrypt
+    npm install --update-binary -f bcrypt
+    cp -r node_modules/bcrypt npm/node_modules/meteor/npm-bcrypt/node_modules/bcrypt
   fi
 fi
 
+cd /bundle/bundle/
 # Set a delay to wait to start meteor container
 if [[ $DELAY ]]; then
   echo "Delaying startup for $DELAY seconds"
