ac08fe8cf5e2da7134ec34deb0094d60739c3d14
Mgonand <marcos@binpar.com>
2017-01-14 18:05:12 +0100

    nodescreen

--- nodescreen/scripts/init.sh
+++ nodescreen/scripts/init.sh
@@ -2,6 +2,7 @@ set -e
 
 bash $METEORD_DIR/lib/install_base.sh
 bash $METEORD_DIR/lib/install_node.sh
+bash $METEORD_DIR/lib/install_forever.sh
 bash $METEORD_DIR/lib/install_xvfb.sh
 bash $METEORD_DIR/lib/install_graphicsmagick.sh
 bash $METEORD_DIR/lib/install_imagemagick.sh
