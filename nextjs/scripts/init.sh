70866e6682172117605e6ea88f40c31e89e727bb
Marcos González Andrés <marcos@binpar.com>
2017-09-19 16:18:57 +0200

    Update init.sh

--- nextjs/scripts/init.sh
+++ nextjs/scripts/init.sh
@@ -1,6 +1,7 @@
 set -e
 
 bash $METEORD_DIR/lib/install_base.sh
+bash $METEORD_DIR/lib/install_libs.sh
 bash $METEORD_DIR/lib/install_node.sh
 bash $METEORD_DIR/lib/install_phantomjs.sh
 bash $METEORD_DIR/lib/install_graphicsmagick.sh
