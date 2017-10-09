8898f97e925518cd60f7032723abb7bb43f91760
Mgonand <marcos@binpar.com>
2017-10-09 18:58:51 +0200

    NextJS Converter dockerimage

---
+++ nextjsconverter/scripts/lib/cleanup.sh
@@ -0,0 +1,25 @@
+## Borrowed from: https://github.com/chriswessels/meteor-tupperware
+
+set -e
+# Autoremove any junk
+apt-get autoremove -y
+
+# Clean out docs
+rm -rf /usr/share/doc /usr/share/doc-base /usr/share/man /usr/share/locale /usr/share/zoneinfo
+
+# Clean out package management dirs
+rm -rf /var/lib/cache /var/lib/log
+
+# Clean out /tmp
+rm -rf /tmp/*
+
+# Clear npm cache
+npm cache clear
+
+cd ~
+rm -rf "poppler-0.53.0.tar.xz"
+rm -rf "poppler-0.53.0"
+rm -rf "fontforge"
+rm -rf "pdf2htmlEX"
+rm -rf "ttf2woff-0.14"
+rm -rf "GraphicsMagick-1.3.19"
