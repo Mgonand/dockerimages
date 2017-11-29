3c0228a49bf28978a25ba6cb7fe8d8a6e6a18e54
miguelBinpar <miguel@binpar.com>
2017-11-29 20:39:10 +0100

    epub2txt.sh

--- nextjsconverter/scripts/init.sh
+++ nextjsconverter/scripts/init.sh
@@ -9,4 +9,5 @@ bash $METEORD_DIR/lib/install_poppler.sh
 bash $METEORD_DIR/lib/install_fontforge.sh
 bash $METEORD_DIR/lib/install_pdf2htmlex.sh
 bash $METEORD_DIR/lib/install_ttf2woff.sh
+bash $METEORD_DIR/lib/install_epub2txt.sh
 bash $METEORD_DIR/lib/cleanup.sh
