d5d8747e41b46326654446c731a24e2cdfd8527f
Marcos González Andrés <marcos@binpar.com>
2018-05-04 22:48:30 +0200

    fix node install

--- nextjs/scripts/lib/install_node.sh
+++ nextjs/scripts/lib/install_node.sh
@@ -12,7 +12,7 @@ if [ -n "$MUPX_NODE_VERSION" ]; then
   mv ${NODE_DIST} /opt/nodejs
 else
   LINK_NODE8=http://nodejs.org/dist/latest-v8.x/
-  DOWNLOAD_NODE8=$(curl -s $LINK_NODE8 | grep -o '<a .*href=.*>' | sed -e 's/<a /\<a /g' | sed -e 's/<a .*href=['"'"'"$
+  DOWNLOAD_NODE8=$(curl -s $LINK_NODE8 | grep -o '<a .*href=.*>' | sed -e 's/<a /\<a /g' | sed -e 's/<a .*href=['"'"'"]//' -e 's/["'"'"'].*$//' -e '/^$/ d' | grep 'linux' | grep "x64" | grep ".tar.gz")
   LINK_NODE8=$LINK_NODE8$DOWNLOAD_NODE8
   curl -O -L $LINK_NODE8
   tar xvzf $DOWNLOAD_NODE8
