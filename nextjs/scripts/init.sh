864a0bea0e0b5a351f0cd04c14797e555fac0535
Mgonand <marcos@binpar.com>
2019-05-21 10:44:59 +0200

    fix scripts

--- nextjs/scripts/init.sh
+++ nextjs/scripts/init.sh
@@ -3,7 +3,6 @@ set -e
 bash $METEORD_DIR/lib/install_base.sh
 bash $METEORD_DIR/lib/install_libs.sh
 bash $METEORD_DIR/lib/install_node.sh
-bash $METEORD_DIR/lib/install_phantomjs.sh
 bash $METEORD_DIR/lib/install_graphicsmagick.sh
 bash $METEORD_DIR/lib/install_imagemagick.sh
 bash $METEORD_DIR/lib/install_ffmpeg.sh
