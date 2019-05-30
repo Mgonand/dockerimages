c237e86259ecd1669267855051fc86f93b283a42
Mgonand <marcos@binpar.com>
2019-05-30 11:29:38 +0200

    fix

--- nextjsconverter-18/scripts/lib/install_base.sh
+++ nextjsconverter-18/scripts/lib/install_base.sh
@@ -2,6 +2,7 @@
 set -e
 apt-get update -y
 apt-get -y upgrade
+apt-get install apt-utils -y
 apt-get install -y build-essential
 apt-get install -y software-properties-common
 apt-get install -y curl git unzip wget bzip2 python
