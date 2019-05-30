c95b79e034cb0604c454362841294fa7b0980e9f
Mgonand <marcos@binpar.com>
2019-05-30 13:23:36 +0200

    test

--- nextjsconverter-18/scripts/lib/install_base.sh
+++ nextjsconverter-18/scripts/lib/install_base.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -e
-apt-get update -y
-apt-get -y upgrade
-apt-get install apt-utils -y
-apt-get install -y build-essential
-apt-get install -y software-properties-common
-apt-get install -y curl git unzip wget bzip2 python
-apt-get install -qq -y cmake gcc libgetopt++-dev git
-apt-get install -qq -y pkg-config libopenjpeg-dev libfontconfig1-dev libfontforge-dev poppler-data poppler-utils poppler-dbg
-apt-get install -qq -y packaging-dev pkg-config python-dev libpango1.0-dev libglib2.0-dev libxml2-dev giflib-dbg
-apt-get install -qq -y libjpeg-dev libtiff-dev uthash-dev libspiro-dev
-apt-get install -y ghostscript
+apt update -y
+apt -y upgrade
+apt install apt-utils -y
+apt install -y build-essential
+apt install -y software-properties-common
+apt install -y curl git unzip wget bzip2 python
+apt install -qq -y cmake gcc libgetopt++-dev git
+apt install -qq -y pkg-config libopenjpeg-dev libfontconfig1-dev libfontforge-dev poppler-data poppler-utils poppler-dbg
+apt install -qq -y packaging-dev pkg-config python-dev libpango1.0-dev libglib2.0-dev libxml2-dev giflib-dbg
+apt install -qq -y libjpeg-dev libtiff-dev uthash-dev libspiro-dev
+apt install -y ghostscript
--- nextjsconverter/scripts/lib/install_base.sh
+++ nextjsconverter/scripts/lib/install_base.sh
@@ -9,7 +9,7 @@ apt-get install -qq -y cmake gcc libgetopt++-dev git
 apt-get install -qq -y pkg-config libopenjpeg-dev libfontconfig1-dev libfontforge-dev poppler-data poppler-utils poppler-dbg
 apt-get install -qq -y packaging-dev pkg-config python-dev libpango1.0-dev libglib2.0-dev libxml2-dev giflib-dbg
 apt-get install -qq -y libjpeg-dev libtiff-dev uthash-dev libspiro-dev
-apt-add-repository ppa:anton+/photo-video-apps -y
-apt-get update -y
+#apt-add-repository ppa:anton+/photo-video-apps -y
+#apt-get update -y
 apt-get install -y pdftk
 apt-get install -y ghostscript
