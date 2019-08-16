5774dd5fac585de2edc2f759291b9a2bc5a235d9
Mgonand <marcos@binpar.com>
2019-08-16 18:25:06 +0200

    apt-get install pdf2htmlex & npm i -g ttf2woff

--- nextjsconverter-16/scripts/lib/install_pdf2htmlex.sh
+++ nextjsconverter-16/scripts/lib/install_pdf2htmlex.sh
@@ -1,9 +1,2 @@
 #!/bin/bash
-cd ~/
-git clone --depth 1 https://github.com/coolwanglu/pdf2htmlEX.git
-cd pdf2htmlEX/
-cmake -ENABLE_LIBJPEG = ON .
-make
-sudo make install
-echo 'export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH' >> ~/.bashrc
-source ~/.bashrc
+apt-get install -y pdf2htmlex
--- nextjsconverter-16/scripts/lib/install_ttf2woff.sh
+++ nextjsconverter-16/scripts/lib/install_ttf2woff.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
-cd ~
-curl -O -L http://wizard.ae.krakow.pl/~jb/ttf2woff/ttf2woff-0.14.tar.gz
-tar xvzf ttf2woff-0.14.tar.gz
-cd ttf2woff-0.14
-make
-cp ttf2woff /usr/bin/
+npm install -g ttf2woff
